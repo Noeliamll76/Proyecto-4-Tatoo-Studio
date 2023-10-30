cd12b1ee4550c71beda0808d527f4184cdb1e2d0
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-31 00:52:26 +0100

    tattoo_artistController register login and getAll

--- src/routes/tattoo_artistRoutes.ts
+++ src/routes/tattoo_artistRoutes.ts
@@ -9,10 +9,6 @@ import { isSuperAdmin } from "../middlewares/isSuperAdmin";
 const router = Router()
 
 router.post('/register', auth, isSuperAdmin, register) 
-// router.post('/login', login)
-// router.get('/profile', auth, profile)
-// router.put('/updateUserByToken', auth, updateUserByToken)
-// router.delete('/deleteById/:id', auth, isSuperAdmin, deleteById)
 router.post('/register', register) 
 router.post('/login', login)
 router.get('/getAllTattooArtist', auth, isSuperAdmin, getAllTattooArtist)
