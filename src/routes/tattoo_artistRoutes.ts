09952f3034900e7b5407863500b1b3273f37211e
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-10-30 23:23:53 +0100

    feat. improvements in tattoo_artistController

--- src/routes/tattoo_artistRoutes.ts
+++ src/routes/tattoo_artistRoutes.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { register} from "../controllers/tattoo_artistController";
   //  , login, profile, updateUserByToken, deleteById
 
-//import { auth } from "../middlewares/auth";
-//import { isSuperAdmin } from "../middlewares/isSuperAdmin";
+import { auth } from "../middlewares/auth";
+import { isSuperAdmin } from "../middlewares/isSuperAdmin";
 
 
 const router = Router()
 
-router.post('/register', register) 
+router.post('/register', auth, isSuperAdmin, register) 
 // router.post('/login', login)
 // router.get('/profile', auth, profile)
 // router.put('/updateUserByToken', auth, updateUserByToken)
